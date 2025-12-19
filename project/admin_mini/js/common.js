525666a99674670344c92864d77e7fe7adb8602b
unkn0wn-j <wjsrnr470@gmail.com>
2025-12-19 17:51:50 +0900

    remove admin

--- project/admin_mini/js/common.js
+++
@@ -1,8 +0,0 @@
-// common.js
-document.addEventListener("DOMContentLoaded", () => {
-  const isDark = localStorage.getItem("darkMode") === "on";
-
-  if (isDark) {
-    document.body.classList.add("dark");
-  }
-});
